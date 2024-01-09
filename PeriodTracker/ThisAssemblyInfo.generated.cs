0ecf018702552ecf6e09d700832d030f74ed36a8
E. Keys <e.t.keys@gmail.com>
2024-01-09 18:57:46 -0500

    provide custome implementation for IAppInfo
    Wanted to be able to provide more descriptive version information.

---
+++ PeriodTracker/ThisAssemblyInfo.generated.cs
@@ -0,0 +1,12 @@
+﻿namespace PeriodTracker;
+
+public partial class ThisAssemblyInfo
+{
+
+    private const string __AndoidBuildNumber = "1";
+    private const string __GitCommitHash = "bd6a14a";
+    private const string __PreReleaseMoniker = "alpha";
+    private const string __Revision = "1";
+    private const string __Version = "0.1.0";
+
+}
--- PeriodTracker/ViewModels/AboutViewModel.cs
+++ PeriodTracker/ViewModels/AboutViewModel.cs
@@ -5,7 +5,11 @@ namespace PeriodTracker.ViewModels;
 public partial class AboutViewModel: ViewModelBase
 {
 
+    public AboutViewModel(IAppInfo appInfo){
+        DisplayVersionText = appInfo.VersionString;
+    }
+
     [ObservableProperty]
-    private string _displayVersionText = "?.?.?-alpha (sha1)";
+    private string _displayVersionText = string.Empty;
 
 }
